542e37bbf204ff5b1515f3b6513cf4a7f86a3701
mariannavalekova <131619121+mariannavalekova@users.noreply.github.com>
2024-10-23 09:20:27 +0200

    Initial commit

---
+++ projekt_zp/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="app-container">
+    <Navigation />
+    <main class="main-content">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { RouterView } from 'vue-router';
+import Navigation from './components/Navigation.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components: {
+    Navigation,
+    Footer
+  }
+}
+</script>
---
+++ projekt_zp/vite.config.js
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  base: process.env.NODE_ENV === 'production'
+    ? '/dist/' // set the base to '/dist/' when in production mode
+    : '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/Backend': {
+        target: 'http://localhost',
+        changeOrigin: true,
+      },
+    },
+  },
+})
