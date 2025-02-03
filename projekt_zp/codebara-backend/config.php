33bb7adccf675936c2c0cda5ef9a6f4e6b67dbc2
mariannavalekova <131619121+mariannavalekova@users.noreply.github.com>
2025-02-03 21:49:36 +0100

    Backend: signin and register APIs

---
+++ projekt_zp/codebara-backend/config.php
@@ -0,0 +1,47 @@
+<?php
+class Database {
+    private $host;
+    private $port;
+    private $user;
+    private $password;
+    private $database;
+    private $conn;
+
+    public function __construct($host, $port, $user, $password, $database) {
+        $this->host = $host;
+        $this->port = $port;
+        $this->user = $user;
+        $this->password = $password;
+        $this->database = $database;
+    }
+
+    public function connect() {
+        try {
+            $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->database;charset=utf8mb4";
+            $this->conn = new PDO($dsn, $this->user, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
+
+    public function close() {
+        $this->conn = null; // Closing the connection
+    }
+}
+
+function connect_to_database() {
+    $database = new Database('127.0.0.1', 3306,'root', '', 'codebara');
+    $connection = $database->connect();
+    
+    return $connection;
+}
+
+function close_database_connection($connection) {
+    $connection = null;
+}
+?>
+
+
